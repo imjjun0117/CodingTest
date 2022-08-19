ee0e9cb848f7879ae013e5179aad2a63bbcd8d44
imjjun0117 <imjjun0117@naver.com>
2022-08-19 16:28:20 +0900

    day0819

--- src/test/day0818/Test1157.java
+++ src/test/day0818/Test1157.java
@@ -6,8 +6,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Test1157 {
-
-	
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
---
+++ src/test/day0818/Test2908.java
@@ -0,0 +1,34 @@
+package test.day0818;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Test2908 {
+	
+	public static int change(String A) {
+		String B = ""+A.charAt(A.length()-1);
+		for(int i=1; i < A.length()-1; i++) {
+			B += A.charAt(i);
+		}//end for
+		B += A.charAt(0);
+		return Integer.parseInt(B);
+	}//change
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk = new StringTokenizer(br.readLine()," ");
+		String A = stk.nextToken();
+		String B = stk.nextToken();
+		int changeA = change(A);
+		int changeB = change(B);
+		if(changeA > changeB) {
+			System.out.println(changeA);
+		} else{
+			System.out.println(changeB);
+		}//end else
+		
+	}//main
+	
+}//class
